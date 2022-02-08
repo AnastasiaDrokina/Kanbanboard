0cde44498bde1f1ac534fe7d20566a2c08ff51c8
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2022-02-08 16:46:31 +0100

    Add a task

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 const form = document.querySelector(".add-task__form");
-const task = document.getElementById("add-task");
-const btnAddTask = document.querySelector(".add-task__button");
-
-console.log(form);
+const taskboardList = document.querySelector(".taskboard__list");
 
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
-});
 
-btnAddTask.addEventListener("click", function () {
-  const taskValue = task.value;
-  console.log(taskValue);
+  const formData = new FormData(evt.target);
+  const addTask = formData.get("task-name");
+
+  // Getting the task value
+  const taskboard = {
+    value: addTask,
+  };
+
+  // Template for a new task
+  const taskboardItem = `
+  <div class="taskboard__item task">
+    <div class="task__body">
+      <p class="task__view">${taskboard.value}</p>
+      <input class="task__input" type="text" value="${taskboard.value}">
+    </div>
+    <button class="task__edit" type="button" aria-label="Изменить"></button>
+  </div>
+  `;
+
+  // Adding a new task to the end of the list
+  taskboardList.insertAdjacentHTML("beforeend", taskboardItem);
+
+  // Reset the input field of form
+  form.reset();
 });
