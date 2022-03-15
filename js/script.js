31fa9cceaf3f5759653ff02093c4449842062bc3
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2022-03-15 17:58:04 +0100

    Deleting tasks

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const form = document.querySelector(".add-task__form");
 const taskboardList = document.querySelector(".taskboard__list");
 const taskEditBtns = document.querySelectorAll(".task__edit");
 const taskInputs = document.querySelectorAll(".task__input");
+const tasksLists = document.querySelectorAll(".taskboard__list");
+const taskItems = document.querySelectorAll(".taskboard__item");
+const btnClear = document.querySelector(".taskboard__button");
+const trashList = document.querySelector(".taskboard__list--trash");
+const emptyTrash = document.querySelector(".task--empty-trash");
 
 function enableTasks() {
   taskEditBtns.forEach((btn) => {
@@ -73,4 +78,65 @@ taskInputs.forEach((input) => {
       enableTasks();
     }
   });
+});
+
+// Drag & Drop
+
+// taskItems.forEach((item) => {
+//   item.draggable = true;
+//   item.addEventListener("dragstart", () => {
+//     item.classList.add("task--dragged");
+//   });
+
+//   item.addEventListener("dragend", () => {
+//     item.classList.remove("task--dragged");
+//   });
+// });
+
+// tasksLists.forEach((list) => {
+//   list.addEventListener("dragover", (evt) => {
+//     evt.preventDefault;
+//     const afterElement = getDragAfterElement(list, evt.clientY);
+//     const activeElement = document.querySelector(".task--dragged");
+//     list.appendChild(activeElement);
+//   });
+// });
+
+// function getDragAfterElement(list, y) {
+//   const draggableElements = [
+//     ...list.querySelectorAll("draggable:not(.task--dragged)"),
+//   ];
+//   draggableElements.reduce((closest, child) => {
+//     const box
+//   }, { offset: Number.POSITIVE_INFINITY);
+// }
+
+function handleEmptyTrashBtn() {
+  const trashItems = trashList.querySelectorAll(".taskboard__item");
+
+  // Hiding block "Basket is empty"
+  if (trashItems.length) {
+    emptyTrash.classList.add("hidden-block");
+
+    // Disabled button "Clear the basket"
+  } else {
+    emptyTrash.classList.remove("hidden-block");
+    btnClear.setAttribute("disabled", "disabled");
+  }
+}
+
+// Deleting cards
+function handleEmptyTrashItems() {
+  const trashItems = trashList.querySelectorAll(".taskboard__item");
+
+  trashItems.forEach((item) => {
+    item.remove();
+  });
+}
+
+handleEmptyTrashBtn();
+
+btnClear.addEventListener("click", () => {
+  handleEmptyTrashItems();
+  handleEmptyTrashBtn();
 });
